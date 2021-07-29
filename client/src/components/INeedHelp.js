15faa5bcea6a64d5c6a3e9a36903bce131c8f427
SHILPA <andavarapushilpa95@gmail.com>
2021-07-29 16:57:33 +0200

    i need help

--- client/src/components/INeedHelp.js
+++ client/src/components/INeedHelp.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "../axios";
 export default function INeedHelp() {
     const [locations, setLocations] = useState([]);
-    const [value, setValues] = useState("S");
+    const [query, setQuery] = useState({});
     // const [results,setResults] = useState()
     useEffect(() => {
         axios.get("/api/locations").then((response) => {
@@ -13,25 +13,29 @@ export default function INeedHelp() {
 
     function getValuesFromLocations() {
         return locations.map((location) => {
-            console.log("location", location);
+            console.log("location-react-16", location);
             return (
-                <option key={location.id} value={location}>
-                    {location}
+                <option key={location.id} value={location.id}>
+                    {location.name}
                 </option>
             );
         });
     }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        // axios.get("/api/search").then((response)=>{
-        //     console.log("[/api/search]-react",response.data);
-        // })
-        console.log("submitted!");
+    function handleChange(event) {
+        console.log("event.target.value", event.target.value);
+        setQuery({
+            ...query,
+            [event.target.name]: event.target.value,
+        });
     }
 
-    function selectValuesFromDropDown(event) {
-        setValues(event.target.value);
+    function handleSubmit(event) {
+        event.preventDefault();
+        axios.get("/api/search", { params: query }).then((response) => {
+            console.log("[response.data-api/search]", response.data);
+        });
+        console.log("submitted!");
     }
 
     return (
@@ -39,17 +43,24 @@ export default function INeedHelp() {
             <h1>I need help</h1>
             <form onSubmit={handleSubmit}>
                 <label>From</label>
-                <select>{getValuesFromLocations}</select>
+                <select name="origin_id" onChange={handleChange}>
+                    {getValuesFromLocations()}
+                </select>
+
                 <label>To</label>
-                <select>{getValuesFromLocations}</select>
+                <select name="destination_id" onChange={handleChange}>
+                    {getValuesFromLocations()}
+                </select>
+
                 <label>Package-Size</label>
-                <select value={value} onChange={selectValuesFromDropDown}>
+                <select onChange={handleChange} name="size">
                     <option value="small">S</option>
                     <option value="medium">M</option>
                     <option value="large">L</option>
                 </select>
+
                 <label>Time-Slot</label>
-                <select onChange={selectValuesFromDropDown}>
+                <select onChange={handleChange} name="time_slot">
                     <option value="daily">Daily</option>
                     <option value="weekend">Weekend</option>
                     <option value="flexible">Flexible</option>
